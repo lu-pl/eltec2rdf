2ff618946bc176c94cd658be872a7751570fd389
Lukas Plank <lupl@tuta.io>
2023-11-28 13:13:23 +0100

    Implemented mkuri for CLSCor URI construction

--- eltec2rdf/utils/utils.py
+++ eltec2rdf/utils/utils.py
@@ -1,7 +1,13 @@
 """Miscellaneous utilies for eltec2rdf."""
 
+import hashlib
+
 from collections.abc import Callable
 from typing import TypeVar
+from uuid import uuid4
+
+from rdflib import URIRef
+from lodkit.utils import genhash
 
 
 T = TypeVar("T")
@@ -21,3 +27,26 @@ def _or(*operands: Callable[..., T],
     for operand in operands:
         if result := operand(*args, **kwargs):
             return result
+
+
+def mkuri(
+        hash_value: str | None = None,
+        length: int | None = 10,
+        hash_function: Callable = hashlib.sha256
+) -> URIRef:
+    """Create a CLSCor entity URI.
+
+    If a hash value is give, the path is generated using
+    a hash function, else the path is generated using a uuid4.
+    """
+    _base_uri: str = "https://clscor.io/entity/"
+    _path: str = (
+        str(uuid4()) if hash_value is None
+        else genhash(
+                hash_value,
+                length=length,
+                hash_function=hash_function
+        )
+    )
+
+    return URIRef(f"{_base_uri}{_path[:length]}")
